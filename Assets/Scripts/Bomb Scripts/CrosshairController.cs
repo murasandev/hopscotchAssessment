2668ddd6574ff6d8bc806ef178048d0b9e11034e
murasandev <dmurayama93@gmail.com>
2023-05-26 15:12:06 -1000

    created bomb crosshair animation

---
+++ Assets/Scripts/Bomb Scripts/CrosshairController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairController : MonoBehaviour
+{
+    public SpriteRenderer xhairOuter;
+    public SpriteRenderer xhairMid;
+    public SpriteRenderer xhairInner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
