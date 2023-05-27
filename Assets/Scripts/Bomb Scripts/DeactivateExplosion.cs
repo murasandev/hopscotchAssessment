67634032561812a33a73944f1eb5de1dff69ad6d
murasandev <dmurayama93@gmail.com>
2023-05-27 12:37:59 -1000

    set up bomb explosion ps

---
+++ Assets/Scripts/Bomb Scripts/DeactivateExplosion.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateExplosion : MonoBehaviour
+{
+    private float timeToActive = 3.0f;
+    private float timer;
+
+    private void OnEnable()
+    {
+        timer = 0f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeToActive)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
